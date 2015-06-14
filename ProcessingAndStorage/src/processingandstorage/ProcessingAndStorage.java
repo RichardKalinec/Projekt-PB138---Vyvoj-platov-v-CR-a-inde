536cf67a3f580034c12778760050b2d06b9ade07
RichardKalinec <rkalinec@gmail.com>
2015-06-14 22:47:00 +0200

    Further progress on ProcessingAndStorage

--- ProcessingAndStorage/src/processingandstorage/ProcessingAndStorage.java
+++ ProcessingAndStorage/src/processingandstorage/ProcessingAndStorage.java
@@ -8,7 +8,10 @@ import java.util.List;
 import java.util.logging.Logger;
 import org.basex.core.Context;
 import org.basex.core.BaseXException;
+import org.basex.core.cmd.Close;
 import org.basex.core.cmd.Open;
+import org.basex.core.cmd.Optimize;
+import org.basex.core.cmd.XQuery;
 
 /**
  * Class ProcessingAndStorage processes data about salaries from given TSV
@@ -66,10 +69,32 @@ public class ProcessingAndStorage {
             return;
         }
 
+        //optimize the database to refresh the index structures
+        new Optimize().execute(context);        
+        //close the database with salaries
+        new Close().execute(context);
         //close the database system
         context.close();
     }
     
+    /**
+     * Method processAndStoreEurostatSalaries() processes TSV files from
+     * Eurostat containing data about salaries with attribute descriptions
+     * in the first column of the forst row, attribute values in the first
+     * column of the other rows, season (usually a year) in the remaining
+     * columns of the forst line, and data about salaries (which are filtered
+     * for digits and dots and only cells containing these characters are
+     * actually processed) are in the rest of the file. If the record of the
+     * salary with given attributes already exists, it is only updated with
+     * the new data (which can include inserting data for another seasons), or
+     * it is newly created, inserted and filled with the new data, if it
+     * doesn't.
+     * 
+     * @param file A file from Eurostat to process. Data about salaries
+     *             formatted as aforementioned is expected.
+     * @param context Database context.
+     * @throws IOException 
+     */
     private static void processAndStoreEurostatSalaries(String file, Context context) throws IOException
     {
         //load file content separated into lines
@@ -102,31 +127,56 @@ public class ProcessingAndStorage {
         //prepare temporary variables
         String[] attValues = null;
         String filteredNumericCell = "";
-        char character = 0;
         //process data from the file and store it into the database
         for(int lineNumber = 1; lineNumber < dataLines.size(); lineNumber++)
         {
             //split line into cells
             dataCells = dataLines.get(0).split("\t");
             
-            
+            //load attribute values from the first cell of the line
             attValues = dataCells[0].split(",");
+            //search the rest of the line for numeric values
             for(int columnNumber = 1; columnNumber < dataCells.length; columnNumber++)
             {
-                filteredNumericCell = "";
-                for(int i = 0; i < dataCells[columnNumber].length(); i++)
-                {
-                    character = dataCells[columnNumber].charAt(i);
-                    if(Character.isDigit(character) || (character == '.'))
-                    {
-                        filteredNumericCell += character;
-                    }
-                }
+                filteredNumericCell = filterStringForDigitsAndDots(dataCells[columnNumber]);
+                
+                //if a numeric value is found, store it in the database in the appropriate way
                 if(!filteredNumericCell.isEmpty())
                 {
-                    
+                    //ckeck if salary record with these attributes is already in the database
+                    String isAlreadyPresentQuery = "let $salaries := doc('Salaries')/salaries" +
+                        "for $salary in $salaries" +
+                        "where $salary/@";
+                    //if it isn't, insert it onto the database
+                    if(new XQuery()
+                    //proceed with processing the rest of the line 
+                    for(; columnNumber < dataCells.length; columnNumber++)
+                    {
+                        new
+                    }
+                    break;
                 }
             }
         }
+    }
+    
+    private static void processAndStoreCSUSalaries(String file, Context context) throws IOException
+    {
+        
+    }
+    
+    private static String filterStringForDigitsAndDots(String input)
+    {
+        char character = 0;
+        String output = "";
+        for(int i = 0; i < input.length(); i++)
+        {
+            character = input.charAt(i);
+            if(Character.isDigit(character) || (character == '.'))
+            {
+                output += character;
+            }
+        }
+        return output;
     }
 }
