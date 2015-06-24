94da9391debbebd8ba8e0f420f70c34b822b38de
RichardKalinec <rkalinec@gmail.com>
2015-06-24 23:03:45 +0200

    Change filename in CreateDatabase from "salariesInit.xml" to "salaries.xml"

--- CreateDatabase/src/createdatabase/CreateDatabase.java
+++ CreateDatabase/src/createdatabase/CreateDatabase.java
@@ -1,7 +1,8 @@
 package createdatabase;
 
-import org.basex.core.*;
-import org.basex.core.cmd.*;
+import org.basex.core.Context;
+import org.basex.core.BaseXException;
+import org.basex.core.cmd.CreateDB;
 
 /**
  * Class CreateDatabase creates a new database named "Salaries". It uses BaseX
@@ -15,13 +16,15 @@ public class CreateDatabase {
 
     /**
      * Runs all the code of the class directly (see class description).
+     * 
      * @param args (ignored) the command line arguments
      * @throws BaseXException if a database command fails
      */
     public static void main(String[] args) throws BaseXException {
         Context context = new Context();
         
-        new CreateDB("Salaries", "src/salariesInit.xml").execute(context);
+        new CreateDB("Salaries", "src/salaries.xml").execute(context);
+        
+        context.close();
     }
-    
 }
